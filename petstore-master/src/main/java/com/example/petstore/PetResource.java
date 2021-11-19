201887e421506f7bec6144561ffdf2b1f418c1fb
hashencooray <hashenrashminda@gmail.com>
2021-11-20 00:18:37 +0530

    final updates

--- petstore-master/src/main/java/com/example/petstore/PetResource.java
+++ petstore-master/src/main/java/com/example/petstore/PetResource.java
@@ -3,49 +3,31 @@ package com.example.petstore;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
-@Path("/v1/pets")
+@Path("/pets")
 @Produces("application/json")
 public class PetResource {
+	List<Pet> pets = new ArrayList<Pet>();
+	List<PetType> petTypes = new ArrayList<>();
+	//Pet Id initializing
+	int petId = 1;
+	//PetType Id initializing
+	int petTypeId=1;
 
 	@APIResponses(value = {
 			@APIResponse(responseCode = "200", description = "All Pets", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(ref = "Pet"))) })
 	@GET
 	public Response getPets() {
-		List<Pet> pets = new ArrayList<Pet>();
-		Pet pet1 = new Pet();
-		pet1.setPetId(1);
-		pet1.setPetAge(3);
-		pet1.setPetName("Boola");
-		pet1.setPetType("Dog");
-
-		Pet pet2 = new Pet();
-		pet2.setPetId(2);
-		pet2.setPetAge(4);
-		pet2.setPetName("Sudda");
-		pet2.setPetType("Cat");
-
-		Pet pet3 = new Pet();
-		pet3.setPetId(3);
-		pet3.setPetAge(2);
-		pet3.setPetName("Peththappu");
-		pet3.setPetType("Bird");
-
-		pets.add(pet1);
-		pets.add(pet2);
-		pets.add(pet3);
 		return Response.ok(pets).build();
 	}
 
@@ -58,17 +40,158 @@ public class PetResource {
 		if (petId < 0) {
 			return Response.status(Status.NOT_FOUND).build();
 		}
-		Pet pet = new Pet();
-		pet.setPetId(petId);
-		pet.setPetAge(3);
-		pet.setPetName("Buula");
-		pet.setPetType("Dog");
+		else {
+			for(int i=0; i<pets.size(); i++){
+				if (petId == pets.get(i).getPetId()){
+					return Response.ok(pets.get(i)).build();
+				}
+			}
 
-		return Response.ok(pet).build();
+		}
 
-
-		
+		return Response.status(Status.NOT_FOUND).build();
 	}
+	@GET
+	@Path("/type/all")
+	public Response getAllPetTypes(){
+		return Response.ok(petTypes).build();
+	}
+
+	@POST
+	@Path("/add")
+	public Response addPet(@RequestBody Pet pet){
+		pet.setPetId(petId);
+		petId++;
+		pets.add(pet);
+		return Response.ok(pet).build();
+	}
+
+	@POST
+	@Path("/type/add")
+	public Response addCategory (@RequestBody PetType type ){
+		type.setTypeId(petTypeId);
+		petTypeId++;
+		petTypes.add(type);
+		return Response.ok(petTypes).build();
+	}
+
+	@DELETE
+	@Path("/delete/{petId}")
+	public  Response deletePet(@PathParam("petId") int petId){
+		if (petId < 0) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		else{
+			for (int i=0; i<pets.size(); i++){
+				if (petId == pets.get(i).getPetId()){
+					pets.remove(i);
+					return Response.ok(pets).build();
+				}
+			}
+			return Response.status(Status.NOT_FOUND).build();
+		}
+	}
+
+	@DELETE
+	@Path("/type/delete/{typeId}")
+	public Response deleteType(@PathParam("typeId") int typeId ){
+		if (typeId < 0) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		else{
+			for (int i=0; i<petTypes.size(); i++){
+				if (typeId == petTypes.get(i).getTypeId()){
+					String type = petTypes.get(i).getPetType();
+					petTypes.remove(i);
+					for (int j=0; j< pets.size(); j++){
+						if(type.equals(pets.get(j).getPetType())){
+							pets.remove(j);
+							j=j-1;
+						}
+					}
+					return Response.ok(petTypes).build();
+
+				}
+			}
+			return Response.status(Status.NOT_FOUND).build();
+		}
+	}
+
+	@PUT
+	@Path("/update/{petId}")
+	public Response updatePet(@PathParam("petId") int petId,@RequestBody Pet pet){
+		if (petId < 0) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		else{
+			for (int i=0; i<pets.size(); i++){
+				if (petId == pets.get(i).getPetId()){
+					if (pet.getPetAge()!=null){
+						pets.get(i).setPetAge(pet.getPetAge());
+					}
+					if (pet.getPetName()!=null){
+						pets.get(i).setPetName(pet.getPetName());
+					}
+					if (pet.getPetType()!=null){
+						pets.get(i).setPetType(pet.getPetType());
+					}
+					return Response.ok(pets.get(i)).build();
+
+				}
+			}
+			return Response.status(Status.NOT_FOUND).build();
+		}
+
+	}
+
+	@PUT
+	@Path("type/update/{typeId}")
+	public Response updateType(@PathParam("typeId") int typeId, @RequestBody PetType petType){
+		if (typeId < 0) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		else{
+			for (int i=0; i<petTypes.size(); i++){
+				if (typeId == petTypes.get(i).getTypeId()){
+					String type = petTypes.get(i).getPetType();
+					petTypes.get(i).setPetType(petType.getPetType());
+					for (int j=0; j< pets.size(); j++){
+						if(type.equals(pets.get(j).getPetType())){
+							pets.get(j).setPetType(petType.getPetType());
+						}
+					}
+					return Response.ok(petTypes).build();
+
+				}
+			}
+			return Response.status(Status.NOT_FOUND).build();
+		}
+	}
+
+	@POST
+	@Path("/search")
+	public Response petSearch(@RequestBody Search search){
+		List<Pet> searchList = new ArrayList<>();
+		if (search.getName()!= null){
+			System.out.println("Name++++++++++++");
+			for (int i =0; i<pets.size(); i++){
+				if (pets.get(i).getPetName().contains(search.getName())){
+					searchList.add(pets.get(i));
+				}
+			}
+		}
+		if (searchList.size()==0){
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		else {
+			return Response.ok(searchList).build();
+		}
+	}
+
+
+
+
+
 
 
 
